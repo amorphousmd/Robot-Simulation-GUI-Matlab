33c16166e17af2c728554e01d2118c0916491540
Nguyễn Xuân Minh <minhnguyenxuan855@gmail.com>
2022-12-03 00:24:10 +0700

    Beta 1.0
    Added Pseudo Inverse, Concat Inverse, No Inverse. A radio button for 2 different types of inverse solving

--- forwardUpdateIncrement.m
+++ forwardUpdateIncrement.m
@@ -6,13 +6,13 @@ global theta4Old;
 global endEffectorX;
 global endEffectorY;
 global endEffectorZ;
-theta1Points = linspace(theta1Old, theta1, 20)
-theta2Points = linspace(theta2Old, theta2, 20)
-theta3Points = linspace(theta3Old, theta3, 20)
-theta4Points = linspace(theta4Old, theta4, 20)
+theta1Points = linspace(theta1Old, theta1, 20);
+theta2Points = linspace(theta2Old, theta2, 20);
+theta3Points = linspace(theta3Old, theta3, 20);
+theta4Points = linspace(theta4Old, theta4, 20);
 for i = 1:20
-    forwardUpdate(theta1Points(i), theta2Points(i), theta3Points(i), theta4Points(i))
-    pause(0.0001)
+    forwardUpdate(theta1Points(i), theta2Points(i), theta3Points(i), theta4Points(i));
+    pause(0.0001);
 end
 
 end
